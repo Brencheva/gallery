447d3ae698d39f6673fbac7baf83d11c92c8d094
Brencheva <kristina.brencheva@mail.ru>
2019-11-11 18:46:28 +0700

    Added photos to template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+import { PhotoState } from './store/state';
+import { Observable } from 'rxjs';
+import { Photo } from './interfaces/photo';
+import { selectPhotos } from './store/selectors';
+import { FetchPhotos } from './store/actions';
+import { FormControl, FormGroup } from '@angular/forms';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
-})
-export class AppComponent {
-  title = 'gallery';
+
+@Component(
+  {
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
+  }
+)
+
+export class AppComponent implements OnInit {
+  photos$: Observable<Photo[]> = this.store.pipe(select(selectPhotos));
+
+  formGroup: FormGroup = new FormGroup(
+    {
+      query: new FormControl(''),
+    }
+  );
+
+  constructor(private store: Store<PhotoState>) {
+  }
+
+  ngOnInit() {
+    this.fetchPhotos();
+  }
+
+  fetchPhotos = (): void => {
+    const actionPayload = {
+      query: this.formGroup.controls.query.value
+    };
+
+    this.store.dispatch(new FetchPhotos(actionPayload));
+  };
 }
